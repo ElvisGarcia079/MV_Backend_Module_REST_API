ea862078a4b7a50d27f11ecbf2f64cd12294ce0b
Elvis <elvisgarcia93@gmail.com>
2022-10-21 16:07:36 -0400

    Add all extra files

--- server.js
+++ server.js
@@ -9,17 +9,46 @@ const port = 3011;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.get("/pokemons", async (req, res) => {
-    let pokemon = await Pokemon.findAll();
-    // console.log("This is the Query Object Passed in: " , req.query);
-    
-    res.send(pokemon);
-})
+// Verb
+    // GET - SELECT - (Searching a Resource or Querying)
+    // POST - INSERT - (Creating a Resource)
+    // PUT - UPDATE - (Updating a Resource)
+    // DELETE - DELETE - (Deleting a Resource)
 
-app.get("/trainers", async (req, res) => {
-    let trainer = await Trainer.findAll();
-    res.send(trainer);
-})
+// Noun
+    // Pokemons
+    // Trainers
+
+// Thing #1 to note about Restful API Best Practices. 
+// Your endpoints (the nouns) should be pluralized
+
+// When you think of REQ.QUERY think of FILTERING
+    // (The only thing it's used for is pagination)
+app.get("/pokemons", async (req, res) => {
+    // If you
+    const {type} = req.query;
+    let pokemon = await Pokemon.findAll({where: {type}});
+
+    res.send(pokemon)
+});
+
+// We have been thinking about Object Oriented Programming since Week 5. 
+
+// We learned that with OOP we have the four principles: 
+    // 1. Inheritence
+        // Objects have an IS A relationship
+
+        // Pokemon IS A type -> /pokemons/types
+        // Trainer IS A human -> "/humans/trainers"
+        // Dog IS A animal -> "/animals/dogs"
+
+    
+
+// app.get("/pokemons/:id", async (req, res) => {
+//     console.log(req.params.id)
+//     let pokemon = await Pokemon.findByPk(req.params.id);
+//     res.send(pokemon)
+// });
 
 app.listen(port, () => {
     seed();
